f2c5e158a64e0c9f28d6d547835280bcdc8059bc
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-09-30 14:27:47 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -17,3 +17,17 @@ outputs:
 Access granted
 Access denied
 """
+import time
+import math
+username=(input("Enter username")).strip()
+password=(input("Enter password")).strip()
+count=1
+while username !="admin" or password !=("12345"):
+    print("access denied")
+    username=(input("Enter username")).strip()
+    password=(input("Enter password")).strip()
+    count=count+1
+    if count>3:
+        break
+if username =="admin" and password==("12345"):
+    print("Access granted")
